9e4a09d48f54da29565a14c02d22883ca97ee483
Raforawesome <ubuntuhasnoroblox@gmail.com>
2025-07-09 19:46:12 -0400

    Set up tracing in `main.rs`

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -4,11 +4,17 @@ use axum::{
     Router,
     routing::{get, post},
 };
+use tracing::Level;
 
 const PORT: &str = "0.0.0.0:3000";
 
 #[tokio::main]
 async fn main() {
+    let t_sub = tracing_subscriber::FmtSubscriber::builder()
+        .with_max_level(Level::DEBUG) // initialize max log level
+        .finish();
+    tracing::subscriber::set_global_default(t_sub).unwrap(); // set as global trace sub
+
     let app = Router::new()
         .route("/", get(async || "Hello, world!"))
         .route("/register", post(endpoints::register));
