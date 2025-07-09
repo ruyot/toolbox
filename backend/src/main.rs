09e2668a02b6077b54a30b40e499ee3396348af8
Raforawesome <ubuntuhasnoroblox@gmail.com>
2025-07-08 20:24:19 -0400

    Hello world webserver

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+const PORT: &str = "0.0.0.0:3000";
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/", get(async || "Hello, world!"));
+
+    let listener = tokio::net::TcpListener::bind(PORT).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
 }
