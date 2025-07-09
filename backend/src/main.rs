3e8e8b3f41e8fa39a1735e9100cdcc7cac815c58
Raforawesome <ubuntuhasnoroblox@gmail.com>
2025-07-09 19:36:20 -0400

    Create register endpoint

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,10 +1,17 @@
-use axum::{Router, routing::get};
+mod endpoints;
+
+use axum::{
+    Router,
+    routing::{get, post},
+};
 
 const PORT: &str = "0.0.0.0:3000";
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(async || "Hello, world!"));
+    let app = Router::new()
+        .route("/", get(async || "Hello, world!"))
+        .route("/register", post(endpoints::register));
 
     let listener = tokio::net::TcpListener::bind(PORT).await.unwrap();
     axum::serve(listener, app).await.unwrap();
