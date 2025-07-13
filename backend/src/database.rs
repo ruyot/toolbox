0f8c3bacec1a9d0ae910c531856f94689a5b6a22
Raforawesome <ubuntuhasnoroblox@gmail.com>
2025-07-13 04:36:59 -0400

    sea_orm -> sqlx

--- backend/src/database.rs
+++ backend/src/database.rs
@@ -1,9 +1,8 @@
-use sea_orm::{DatabaseConnection, DbErr};
+use sqlx::{SqlitePool, sqlite::SqlitePoolOptions};
 
 // TODO: change sqlite to real database instance later
 const DB_URL: &str = "sqlite:dev.sqlite?mode=rwc";
 
-pub async fn connect() -> Result<DatabaseConnection, DbErr> {
-    let db = sea_orm::Database::connect(DB_URL).await?;
-    Ok(db)
+pub async fn connect() -> Result<SqlitePool, sqlx::Error> {
+    Ok(SqlitePoolOptions::default().connect(DB_URL).await?)
 }
